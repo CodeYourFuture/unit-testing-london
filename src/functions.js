9d79804127a1f93d4e63428b34051811601697d3
KiyaG <hamilee.kiya@gmail.com>
2017-06-28 10:25:01 +0100

    implemented all the functions and made them pass all the tests

--- src/functions.js
+++ src/functions.js
@@ -1,59 +1,91 @@
 const functions = {
 
-  removeMiddle: function( words ){
-    // words is an array of strings. Remove the middle word from the array and return it
-  },
+    removeMiddle: function(words) {
+        // words is an array of strings. Remove the middle word from the array and return it
+        var middleElementIndex = Math.ceil(words.length / 2) - 1; //index is 0 based
+        console.log(words[middleElementIndex]);
+        return words.splice(middleElementIndex, 1);
 
-  get2ndAnd3rd: function( myArray ){
-    // myArray is an array. Return an array containing 2nd and 3rd items from myArray
-    // make sure myArray remains unchanged.
-  },
+    },
 
-  mapper: function( myArray ){
-    // return a new array which has same all items in myArray incremented by one
-    // make sure myArray remains unchanges
-  },
+    get2ndAnd3rd: function(myArray) {
+        // myArray is an array. Return an array containing 2nd and 3rd items from myArray
+        // make sure myArray remains unchanged.
+        return [myArray[1], myArray[2]]; //array 0 based index
+    },
 
-  wordLengths: function( words ){
-    // the parameter words is an array of words. Return a new array that contains
-    // the number of letters in each word
-  },
+    mapper: function(myArray) {
+        // return a new array which has same all items in myArray incremented by one
+        // make sure myArray remains unchanges
+        return myArray.map(function(item) {
+            return item + 1;
+        });
+    },
 
-  largerThanTen: function( numbers ){
-    // numbers is an array of numbers. return an array that contains all numbers
-    // from the input array which are greater than 10
-  },
+    wordLengths: function(words) {
+        // the parameter words is an array of words. Return a new array that contains
+        // the number of letters in each word
+        return words.map(function(word) {
+            return word.length;
+        });
+    },
 
-  even: function( numbers ){
-    // numbers is an array of numbers. return an array that contains all even numbers
-    // from the input array
-    // hint: you may want to use the modulus operator '%'
-  },
+    largerThanTen: function(numbers) {
+        // numbers is an array of numbers. return an array that contains all numbers
+        // from the input array which are greater than 10
+        return numbers.filter(function(number) {
+            if (number > 10)
+                return number;
+        })
+    },
 
-  findTheNeedle: function( words ){
-    // words is an array of words. Return the index of the word 'needle'
-  },
+    even: function(numbers) {
+        // numbers is an array of numbers. return an array that contains all even numbers
+        // from the input array
+        // hint: you may want to use the modulus operator '%'
+        return numbers.filter(function(number) {
+            if (number % 2 === 0)
+                return number;
+        });
+    },
 
-  findLargest: function( numbers ){
-    // numbers is an array of numbers. return the largest number from that array
-  },
+    findTheNeedle: function(words) {
+        // words is an array of words. Return the index of the word 'needle'
+        return words.indexOf("needle");
+    },
 
-  buildTrain: function( numberOfCarriages ){
+    findLargest: function(numbers) {
+        // numbers is an array of numbers. return the largest number from that array
+        let largestNumber = numbers[0];
+        numbers.forEach(function(element) {
+            if (element > largestNumber)
+                largestNumber = element;
+        });
+        return largestNumber;
+    },
 
-    const Train = function( numberOfCarriages ){
-      this.numberOfCarriages = numberOfCarriages;
+    buildTrain: function(numberOfCarriages) {
+
+        const Train = function(numberOfCarriages) {
+            this.numberOfCarriages = numberOfCarriages;
+        }
+
+        // return a new instance of Train which has the specified number of carriages
+        return new Train(numberOfCarriages);
+    },
+
+    addMethod: function(bookTable) {
+        const restaurant = {
+            freeTables: 7,
+            // make bookTable is a function. make it a method of restaurant
+            bookTable: function() {
+                this.freeTables--;
+                if (this.freeTables < 0) this.freeTables = 0;
+            }
+        };
+
+        return restaurant;
     }
-
-    // return a new instance of Train which has the specified number of carriages
-  },
-
-  addMethod: function( bookTable ){
-    const restaurant = {
-      freeTables: 7
-    };
-
-    // make bookTable is a function. make it a method of restaurant
-
-    return restaurant;
-  }
 };
+
+module.exports = functions;
--- test/functions.test.js
+++ test/functions.test.js
@@ -1,93 +1,94 @@
+var functions = require('../src/functions.js');
 test('Remove middle', () => {
-  const words = [ 'mouse', 'giraffe', 'queen', 'window', 'bottle'];
-  
-  const expectedWords = [ 'mouse', 'giraffe', 'window', 'bottle'];
-  const expectedOutput = [ 'queen' ];
+    const words = ['mouse', 'giraffe', 'queen', 'window', 'bottle'];
 
-  const output = functions.removeMiddle( words );
+    const expectedWords = ['mouse', 'giraffe', 'window', 'bottle'];
+    const expectedOutput = ['queen'];
 
-  expect(output).toEqual(expectedOutput);
-  expect(words).toEqual(expectedWords);
+    const output = functions.removeMiddle(words);
+
+    expect(output).toEqual(expectedOutput);
+    expect(words).toEqual(expectedWords);
 });
 
 test('Get second and third', () => {
-  const numbers = [ 90, 5, 11, 5, 6 ];
-  
-  const expectedOutput = [ 5, 11];
-  const expectedNumbers = [ 90, 5, 11, 5, 6 ];
+    const numbers = [90, 5, 11, 5, 6];
 
-  const output = functions.get2ndAnd3rd( myArray );
+    const expectedOutput = [5, 11];
+    const expectedNumbers = [90, 5, 11, 5, 6];
+    //myArray needs to be changed to numbers
+    const output = functions.get2ndAnd3rd(numbers);
 
-  expect(output).toEqual(expectedOutput);
-  expect(numbers).toEqual(expectedNumbers);
+    expect(output).toEqual(expectedOutput);
+    expect(numbers).toEqual(expectedNumbers);
 });
 
 test('Add 1 to each item in myArray', () => {
-  const myArray = [ 31, 57, 12, 5];
+    const myArray = [31, 57, 12, 5];
 
-  const unchanged = [ 31, 57, 12, 5];
-  const expected = [ 32, 58, 13, 6];
-  const output = functions.mapper( myArray );
+    const unchanged = [31, 57, 12, 5];
+    const expected = [32, 58, 13, 6];
+    const output = functions.mapper(myArray);
 
-  
-  expect(output).toEqual(expected);
-  expect(myArray).toEqual(unchanged);
+
+    expect(output).toEqual(expected);
+    expect(myArray).toEqual(unchanged);
 });
 
 test('Get word lengths', () => {
-  const words = [ 'sun', 'potato', 'roundabout', 'pizza' ];
-  const expected = [ 3, 6, 10, 5 ];
+    const words = ['sun', 'potato', 'roundabout', 'pizza'];
+    const expected = [3, 6, 10, 5];
 
-  const output = functions.wordLengths( words );
-  expect(output).toEqual( expected );
+    const output = functions.wordLengths(words);
+    expect(output).toEqual(expected);
 });
 
 test('Get numbers greater than 10', () => {
-  const numbers = [ 4, 10, 32, 9, 21];
-  const expected = [ 32, 21 ];
+    const numbers = [4, 10, 32, 9, 21];
+    const expected = [32, 21];
 
-  const output = functions.largerThanTen( numbers );
-  expect(output).toEqual( expected );
+    const output = functions.largerThanTen(numbers);
+    expect(output).toEqual(expected);
 });
 
 test('Get even numbers', () => {
-  const numbers = [ 22, 13, 73, 82, 4];
-  const expected = [ 22, 82, 4 ];
+    const numbers = [22, 13, 73, 82, 4];
+    const expected = [22, 82, 4];
 
-  const output = functions.even( numbers );
-  expect(output).toEqual( expected );
+    const output = functions.even(numbers);
+    expect(output).toEqual(expected);
 });
 
 test('Find the needle', () => {
-  const words = [ 'house', 'train', 'slide', 'needle', 'book' ];
-  const expected = 3;
+    const words = ['house', 'train', 'slide', 'needle', 'book'];
+    const expected = 3;
 
-  const output = functions.findTheNeedle( words );
-  expect(output).toEqual( expected );
+    const output = functions.findTheNeedle(words);
+    expect(output).toEqual(expected);
 });
 
 test('Find largest number', () => {
-  const numbers = [ 3, 21, 88, 4, 36];
-  const expected = 88;
+    const numbers = [3, 21, 88, 4, 36];
+    const expected = 88;
 
-  const output = functions.findLargest( numbers );
-  expect(output).toEqual( expected );
+    const output = functions.findLargest(numbers);
+    expect(output).toEqual(expected);
 });
 
 test('Build train', () => {
-  const train = functions.buildTrain( 8 );
+    const train = functions.buildTrain(8);
 
-  expect( train.numberOfCarriages ).toEqual( 8 );  
+    expect(train.numberOfCarriages).toEqual(8);
 });
 
 test('Book a table', () => {
 
-  const bookTable = function(){
-    this.freeTables--;
-    if( this.freeTables < 0 ) this.freeTables = 0;
-  }
+    const bookTable = function() {
+        this.freeTables--;
+        if (this.freeTables < 0) this.freeTables = 0;
+    }
 
-  const restaurant = functions.addMethod( bookTable );
-  restaurant.bookTable();
-  expect( restaurant.freeTables ).toBe( 6 );
+    const restaurant = functions.addMethod(bookTable);
+    restaurant.bookTable();
+    expect(restaurant.freeTables).toBe(6);
 });
