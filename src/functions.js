baf6ca492ac84b921e8842bcc32d3911c9846aa2
chrisofarabia <chrisofarabia@yahoo.co.uk>
2018-02-01 13:30:00 +0000

    All functions complete and passing test suite

--- src/functions.js
+++ src/functions.js
@@ -1,133 +1,169 @@
 function removeMiddle( words ){
-  // words is an array which contains an odd number of strings
-  // return a new array containing only the middle word
-  // the words array should no longer contain the middle word
-  // hint: splice
+  var midWord = Math.floor(words.length / 2);
+  return words.splice(midWord, 1);
+
 }
 
 function get2ndAnd3rd( myArray ){
-  // myArray is an array of numbers
-  // return an array containing the 2nd and 3rd items from myArray
-  // myArray should remain unchanged
-  // hint: slice
+  let newArr = myArray.slice(1, 3);
+  return newArr;
 }
 
 function mapper( myArray ){
-  // myArray is an array of numbers
-  // return a new array which has all items in myArray incremented by one
-  // myArray should remain unchanged
-  // hint: map
+  const newArr = myArray.map(x => x + 1);
+  return newArr;
 }
 
 function wordLengths( words ){
-  // words is an array of strings
-  // return a new array that contains the number of letters in each word
-  // hint: strings have a 'length' property
+  let numArray = [];
+  words.forEach(x => {
+    numArray.push(x.length);
+  })
+  return numArray;
 }
 
-function cities( capitals, formatter ){
-  // capitals is an array of objects that have a city and country property
-  // for example
-  // {
-  //   city: 'Paris',
-  //   country: 'France'
-  // }
-  // formatter is a function that transforms a capital object into a sentence returns it
-  // such as 'Paris is the capital of France'
+function cities( capitalArr, formatter ){
+  let citiesArr = [];
 
-  // apply formatter to each object in capitals array and return an array of resulting sentences
+  capitalArr.forEach(x => {
+    let phrase = formatter(x);
+    citiesArr.push(phrase);
+  })
+
+  return citiesArr;
 }
 
 function largerThanTen( numbers ){
-  // numbers is an array of numbers
-  // return a new array that contains only numbers from the input array which are greater than 10
-  // hint: filter
+  let newArr = numbers.filter(num => num > 10);
+
+  return newArr;
 }
 
 function even( numbers ){
-  // numbers is an array of numbers
-  // return a new array that contains only even numbers from the input array
-  // hint: you may want to use the modulus operator '%'
+  let newArr = [];
+
+  numbers.forEach(num => {
+    if(num % 2 === 0) {
+      newArr.push(num);
+    }
+  })
+
+  return newArr;
 }
 
 function findTheNeedle( words ){
   // words is an array of words
   // return the index of the word 'needle'
   // hint: indexOf
+  let wordIndex = words.indexOf("needle");
+
+  return wordIndex;
 }
 
 function findLargest( numbers ){
   // numbers is an array of numbers
   // return the largest number from that array
+  let maxNum = -Infinity;
+  numbers.forEach(num => {
+    if (num > maxNum) {
+      maxNum = num;
+    }
+  })
+  return maxNum;
 }
 
 function addAllnumbers( numbers ) {
   // numbers is an array of numbers
   // return the sum of all the numbers in the array
+  let arrSum = 0;
+  numbers.forEach(num => {
+    arrSum += num;
+  })
+
+  return arrSum;
 }
 
 function average( things ) {
   // things is an array of numbers and strings
   // return the average of all the numbers
   // be sure to exclude the strings
+  let arrNums = [];
+  let arrSum = 0;
+
+  things.forEach(thing => {
+    if (typeof thing === "number") {
+      arrNums.push(thing);
+    }
+  })
+
+  arrNums.forEach(num => {
+    arrSum += num;
+  })
+
+  return (arrSum / arrNums.length);
 }
 
 function paintShop( cars, colour ){
-  // cars is an array of objects that have 
-  // their properties are `make`, `model` and `colour`
+  let carsClone = [];
 
-  // for example
-  // {
-  //   make: 'Ford',
-  //   model: 'Fiesta',
-  //   color: 'red'
-  // }
+  cars.forEach(car => {
+    carsClone.push(Object.assign({}, car));
+  })
 
-  // set the colour of each Ford car to be the colour 
-  // passed in and return the new array
+  carsClone.forEach(car => {
+    if (car.make === "Ford") {
+      car.colour = colour;
+    }
+  })
 
-  // the original array passed in should not change
-  // hint: look up 'Cloning objects in JavaScript'
+  return carsClone;
 }
 
 function sales( cars ){
-  // cars is an array of objects that have been sold
-  // their properties are `make`, `model`, `colour` and `price`
+  let carSales = {};
 
-  // for example
-  // {
-  //   make: 'Ford',
-  //   model: 'Fiesta',
-  //   color: 'red',
-  //   price: 5999
-  // }
+  cars.forEach(car => {
 
-  // calculate and return the total sales for each make and return the totals
-  // the output should like a bit like
+    if(carSales.hasOwnProperty(car.make)) {
+      carSales[car.make] += car.price;
+    } else {
+      carSales[car.make] = car.price;
+    }
+  })
 
-  // {
-  //   'Ford': 20000,
-  //   'Vauxhall': 15000
-  // }
+  return carSales;
 }
 
 // Harder challenges
-function secondLargest( numbers ){
-  // numbers is an array of numbers
-  // return the index of the second 
-  // largest number in the array
+function secondLargest( arr ){
+  let   largest     = -Infinity,
+  largestIndex      = 0,
+  nextLargest       = -Infinity,
+  nextLargestIndex  = 0;
+
+  for (var i = 0, n = arr.length; i < n; ++i) {
+    var nr = arr[i];                              // convert to number first
+
+    if (nr > largest) {
+      nextLargest = largest;                   // save previous largest value
+      nextLargestIndex = largestIndex;
+      largest = nr;
+      largestIndex = i;
+    } else if (nr < largest && nr > nextLargest) {
+      nextLargest = nr;                        // new second largest value
+      nextLargestIndex = i;
+    }
+  }
+  // console.log(nextLargestIndex);
+  return nextLargestIndex;
 }
 
 function factorial( int ) {
-  // int is an integer
-  // a factorial is the product of all non-negative integers
-  // less than or equal to the iniital number.
-
-  // for example the factorial of 5 is 120
-  // 120 = 1 * 2 * 3 * 4 * 5
-
-  // calculate and return the factorial of int
-  // note: factorial of 0 is 1
+  let fact = 1;
+  for (var i = 1; i < int + 1; i++) {
+    fact = fact * i;
+  }
+  return fact;
 }
 
 module.exports = {
@@ -144,5 +180,6 @@ module.exports = {
   average,
   paintShop,
   sales,
-  secondLargest
+  secondLargest,
+  factorial
 };
